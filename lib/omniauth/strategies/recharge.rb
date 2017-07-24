dbed1846e07353eef24e9216829db5ffb76a406f
Paul Robertson <t.paulrobertson@gmail.com>
2017-07-24 06:58:43 -0400

    override the incorrect redirect_uri implementation

--- lib/omniauth/strategies/recharge.rb
+++ lib/omniauth/strategies/recharge.rb
@@ -30,6 +30,12 @@ module OmniAuth
         api_url = ENV['RECHARGE_API_URL'] || 'https://api.rechargeapps.com/'
         @raw_info ||= access_token.get(api_url).parsed
       end
+
+       def callback_url
+        # make sure the query string doesnt get added to the redirect_uri
+        # https://github.com/intridea/omniauth-oauth2/issues/93
+        full_host + script_name + callback_path
+      end
     end
   end
 end
